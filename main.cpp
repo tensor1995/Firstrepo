03913f98144c6f5bba5dad3b0ed9819fc600027f
tensor1995 <40393038+tensor1995@users.noreply.github.com>
2018-07-14 19:17:33 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,17 @@
+bool checkBST(Node* root) {
+
+       return checkImproved(root,INT8_MIN,INT8_MAX);
+
+	}
+
+   bool checkImproved(Node *node,int8_t min,int8_t max)
+   {
+       if(node == NULL)
+           return true;
+
+       if(node->data >= max || node->data <= min)
+           return false;
+
+       return ( checkImproved(node->left,min,node->data) &&
+               checkImproved(node->right,node->data,max) );
+   }
