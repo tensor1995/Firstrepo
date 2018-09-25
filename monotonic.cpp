24803b872fbb9c1059078267fc95ab248fa114ab
Akash Singha <40393038+tensor1995@users.noreply.github.com>
2018-09-25 14:25:31 +0530

    Leetcode

---
+++ monotonic.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool isMonotonic(vector<int>& A) {
+        
+      bool increasing = true;
+      bool decreasing = true;
+    for (int i=1; i<A.size(); i++){
+        if (A[i-1] > A[i]){
+            increasing = false;
+        } else if(A[i-1] < A[i]){
+            decreasing = false;
+        }
+        if (!increasing && !decreasing){
+            return false;
+        }
+    }
+    return true;
+        
+    }
+};
